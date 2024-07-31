ddbcf3b8175d714d39f3c485701114393cac9c87
pradyumnainguva17 <pradyumna.i@siriab.se>
2024-07-31 19:20:20 +0530

    Update JavaCode2.java with line 2

--- JavaCode2.java
+++ JavaCode2.java
@@ -3,5 +3,6 @@ class JavaCode2
     public static void main(String []args)
     {
         System.out.println("My Second Java Program for Jenkins Webhook");
+        System.out.println("Welcome to Jenkins");
     }
 };
